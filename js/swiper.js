e2c9abcffc74879f03354b9a68e91321acf219c2
kaanchita <kaanchita@gmail.com>
2024-01-29 22:58:39 +0700

    swiper

--- js/swiper.js
+++ js/swiper.js
@@ -13,4 +13,18 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    breakpoints: {
+        360: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
   });

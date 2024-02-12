8b18c8fee2867639a1f341f17505dbea6d371e8f
kaanchita <kaanchita@gmail.com>
2024-02-12 18:50:53 +0700

    gototop

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ $(document).ready(function(){
       $(this).toggleClass('open');
     });
   });
+
+  // go to top
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("goToTop").style.display = "block";
+      document.getElementById("goToTop").style.opacity = 1; /* แสดงปุ่มเมื่อ scroll ลงมา */
+    } else {
+      document.getElementById("goToTop").style.opacity = 0; /* ซ่อนปุ่มเมื่อ scroll ไปด้านบน */
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }

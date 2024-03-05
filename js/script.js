2896e842ffcf4f2ff39039f2315fa580c7b82427
Arthit P <144419746+Arthittaya02-07@users.noreply.github.com>
2024-03-05 11:14:55 +0700

    แก้ล่าวันที่ 4/03/67

--- js/script.js
+++ js/script.js
@@ -94,4 +94,22 @@ window.addEventListener('scroll', function() {
         });
       });
 
+      // poster
+  const hasCookie = document.cookie.includes('popup=closed');
+if (!hasCookie) {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'block';
+
+  const closeButton = document.getElementById('close-button');
+  closeButton.addEventListener('click', () => {
+    document.cookie = 'popup=closed; max-age=300';
+    popup.style.display = 'none';
+  });
+}
+function closePopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
+}
+  
+      
 
